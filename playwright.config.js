3a4c8c339ed03a7704355ccbe07fb00b9b932066
PrakashBalu23 <prakash_b@grhombus.com>
2026-09-03 16:31:33 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -27,11 +27,15 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    browserName: 'chromium',
+    headless: false,
+    screenshot: 'on',
+    trace: 'on',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  
   },
 
   /* Configure projects for major browsers */
